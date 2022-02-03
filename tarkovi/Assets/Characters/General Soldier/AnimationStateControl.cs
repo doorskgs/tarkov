b85db91ec1a825a408dd1eed96fad2b073322115
oboforty <rajmund.csombordi@gmail.com>
2022-02-03 20:12:23 +0100

    Sebastian aim

--- tarkovi/Assets/Characters/General Soldier/AnimationStateControl.cs
+++ tarkovi/Assets/Characters/General Soldier/AnimationStateControl.cs
@@ -24,16 +24,21 @@ public class AnimationStateControl : MonoBehaviour
 {
     public bool controlMoveByVelocity = false;
 
-    [Tooltip("Top Spine bone")]
-    public Transform SpineBoneSkeleton;
+    [Tooltip("Target Helper for aiming")]
+    public Transform AimHelperTransform;
+    public Transform WeaponSlotBaseTransform;
+    float aimHelperFrontDistance;
 
     public Animator anim;
+
+    public Vector3 AimDirection = Vector3.forward;
 
 
     void Awake()
     {
         if (anim == null)
             TryGetComponent(out anim);
+        aimHelperFrontDistance = AimHelperTransform.position.z - WeaponSlotBaseTransform.position.z;
     }
 
     public void SetMoveAnimation(MoveAnimationState state)
@@ -57,12 +62,33 @@ public class AnimationStateControl : MonoBehaviour
 
     private void LateUpdate()
     {
+        // point laser towards direction
+#if UNITY_EDITOR
+        Debug.DrawRay(WeaponSlotBaseTransform.position, AimDirection * 1000f, Color.red);
+#endif
+        // put AimHelper T to direction, so that it drives TPS LookAround IK rig
+        AimHelperTransform.position = WeaponSlotBaseTransform.position + AimDirection * aimHelperFrontDistance;
+        // rotate player towards aim in Y axis
+        //transform.rotation = aim
+
+
+
         if (!controlMoveByVelocity) return;
 
-        
+        // @todo: other stuff?
     }
 
-    public void SetLookAt()
+    public void SetLookAt(Vector3 dir)
+    {
+        AimDirection = dir.normalized;
+    }
+
+    public void SetLookAt(Quaternion dir)
+    {
+
+    }
+
+    public void SetLookAt(Transform target)
     {
 
     }
